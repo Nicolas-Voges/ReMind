ea7c12762efe4fff2eb846dc67ed1f31847d94aa
Nicolas Voges <nicolas.voges@hotmail.com>
2026-06-18 01:14:03 +0200

    feat: add initial flashcard and category models

--- flashcards_app/models.py
+++ flashcards_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Flashcard(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        related_name='flashcards'
+    )
+    question = models.TextField()
+    answer = models.TextField()
+    stage = models.IntegerField(default=1)
+    
+    categories = models.ManyToManyField(
+        Category, 
+        related_name='flashcards', 
+        blank=True
+    )
+
+    def __str__(self):
+        return f"{self.question[:20]}..."
