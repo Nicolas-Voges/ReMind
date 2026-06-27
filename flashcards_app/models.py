a546daddcfc9ab37b881bfcdebb61357d3ae9103
Nicolas Voges <nicolas.voges@hotmail.com>
2026-06-27 05:19:35 +0200

    feat(flashcards): add choices field and card type enumeration
    - Added choices JSONField to Flashcard model
    - Created CardType TextChoices enum and integrated it into Flashcard model
    - Changed default values for choices and search_terms from None to empty lists for JSON compatibility

--- flashcards_app/models.py
+++ flashcards_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     user = models.ForeignKey(
@@ -19,6 +20,12 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class CardType(models.TextChoices):
+    SELF_ASSESSMENT = 'self_assessment', "Self assessment"
+    MULTIPLE_CHOICE = 'multiple_choice', "Multiple choice"
+    TEXT_INPUT = 'text_input', "Text input"
 
 
 class Flashcard(models.Model):
@@ -28,7 +35,9 @@ class Flashcard(models.Model):
         related_name='flashcards'
     )
     question = models.TextField()
-    answer = models.TextField()
+    answer = models.TextField(blank=True)
+    choices = models.JSONField(default=list)
+    card_type = models.CharField(max_length=20, choices=CardType.choices, default=CardType.SELF_ASSESSMENT)
     stage = models.IntegerField(default=1)
     notes = models.TextField(blank=True)
     search_terms = models.JSONField(default=list, blank=True)
