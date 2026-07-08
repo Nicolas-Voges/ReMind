eda94b3169647ce5578c515a6d610c5ec250b93d
Nicolas Voges <nicolas.voges@hotmail.com>
2026-07-08 13:27:07 +0200

    feat: add pagination class for flashcard list

--- flashcards_app/api/views.py
+++ flashcards_app/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 
 from flashcards_app.models import Flashcard
@@ -6,12 +7,19 @@ from flashcards_app.models import Flashcard
 from .serializers import FlashCardModelSerializer
 
 
+class FlashcardPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'size'
+    max_page_size = 100
+
+
 class FlashCardModelViewSet(viewsets.ModelViewSet):
     serializer_class = FlashCardModelSerializer
+    pagination_class = FlashcardPagination
 
     def get_queryset(self):
         user = self.request.user
-        return Flashcard.objects.filter(user=user)
+        return Flashcard.objects.filter(user=user).order_by('pk')
 
     permission_classes = [IsAuthenticated]
 
