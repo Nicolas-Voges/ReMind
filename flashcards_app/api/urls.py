6b2e6a18341df8263e7458c66bbdca258cd44b65
Nicolas Voges <nicolas.voges@hotmail.com>
2026-07-10 23:14:36 +0200

    feat: add model view set for categories and urls

--- flashcards_app/api/urls.py
+++ flashcards_app/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import FlashCardModelViewSet
+from .views import CategoryModelViewSet, FlashCardModelViewSet
 
 router = DefaultRouter()
 router.register(r"flashcards", FlashCardModelViewSet, basename="flashcard")
+router.register(r"categories", CategoryModelViewSet, basename="category")
 
 urlpatterns = [
     path('', include(router.urls)),
--- flashcards_app/api/views.py
+++ flashcards_app/api/views.py
@@ -2,12 +2,12 @@ from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 
-from flashcards_app.models import Flashcard
+from flashcards_app.models import Category, Flashcard
 
-from .serializers import FlashCardModelSerializer
+from .serializers import CategoryModelSerializer, FlashCardModelSerializer
 
 
-class FlashcardPagination(PageNumberPagination):
+class DefaultPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'size'
     max_page_size = 100
@@ -15,13 +15,25 @@ class FlashcardPagination(PageNumberPagination):
 
 class FlashCardModelViewSet(viewsets.ModelViewSet):
     serializer_class = FlashCardModelSerializer
-    pagination_class = FlashcardPagination
+    pagination_class = DefaultPagination
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
         return Flashcard.objects.filter(user=user).order_by('pk')
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class CategoryModelViewSet(viewsets.ModelViewSet):
+    serializer_class = CategoryModelSerializer
+    pagination_class = DefaultPagination
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Category.objects.filter(user=user).order_by('pk')
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
